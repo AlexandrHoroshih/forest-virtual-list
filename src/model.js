80effd2ca7f95f3eb1f1ee63bf51ab6d04005580
AlexandrHoroshih <xad71rus@gmail.com>
2021-04-02 21:18:25 +0300

    chore(model): lint

--- src/model.js
+++ src/model.js
@@ -1,13 +1,13 @@
-import { createStore, createEvent } from "effector";
-import faker from "faker";
+import { createStore, createEvent } from 'effector';
+import faker from 'faker';
 
 const getRandomColor = () =>
-  "#" + Math.floor(Math.random() * 16777215).toString(16);
+  `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 
 const getNext = (n = 10) => {
   const next = [];
 
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i += 1) {
     const fakeData = faker.helpers.createTransaction();
 
     next.push({ ...fakeData, color: getRandomColor() });
@@ -20,7 +20,7 @@ export const next = createEvent();
 export const prev = createEvent();
 export const $list = createStore(getNext(1000));
 
-$list.watch((l) => console.log("list updated", l));
+$list.watch((l) => console.log('list updated', l));
 
 $list.on(next, (list, p) => list.concat(getNext(p)));
 $list.on(prev, (list, p) => getNext(p).concat(list));
